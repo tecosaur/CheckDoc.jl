559324a8c5acbe28b507b03e84603033f2ce48c2
TEC <git@tecosaur.net>
2024-01-30 19:47:09 +0800

    Use StyledStrings for printing

--- src/display.jl
+++ src/display.jl
@@ -1,8 +1,10 @@
-const SEVERITY_COLORS =
-    Dict(1 => :red,        # Error
-         2 => :yellow,     # Warning
-         3 => :light_blue, # Suggestion
-         4 => :green)      # Tip
+using StyledStrings
+
+const SEVERITY_FACES =
+    Dict(1 => :red,         # Error
+         2 => :yellow,      # Warning
+         3 => :bright_blue, # Suggestion
+         4 => :green)       # Tip
 
 const SEVERITY_LABELS =
     Dict(1 => "Error",
@@ -11,45 +13,27 @@ const SEVERITY_LABELS =
          4 => "Tip")
 
 function Base.show(io::IO, ::MIME"text/plain", report::DocsReport)
-    printstyled(io, " ◀▶ Report on ", bold=true)
-    printstyled(io, length(report.checked), bold=true, color=:light_green)
-    printstyled(io, ifelse(length(report.checked) == 1,
-                       " symbol from ",
-                       " symbols from "), bold=true)
-    printstyled(io, string(report.mod), bold=true, color=:light_blue)
-    printstyled(io, " ━ found ", bold=true)
-    let totalissues = mapreduce(length ∘ last, +, report.issues, init=0)
-        printstyled(io, totalissues, bold=true, color=:light_red)
-        printstyled(io, " possible", ifelse(totalissues == 1, " issue", " issues"), bold=true)
-    end
-    print(io, '\n', '═'^70)
+    totalissues = mapreduce(length ∘ last, +, report.issues, init=0)
+    headerline =
+        styled" {bold:◀▶ Report on {bright_green:$(length(report.checked))}\
+               $(ifelse(length(report.checked) == 1, \" symbol from \", \" symbols from \"))\
+               {bright_blue:$(report.mod)} ━ found {bright_red:$totalissues} possible\
+               $(ifelse(totalissues == 1, \" issue\", \" issues\"))}"
+    print(io, headerline, '\n', '═'^textwidth(headerline))
     if !isnothing(report.maxlevel)
-        printstyled(io, " (severity ≥ $(report.maxlevel))", color=:light_black)
+        print(io, styled" {grey:(severity ≥ $(report.maxlevel))}")
     end
-    print('\n')
     for (label, issues) in report.issues
-        print(io, ifelse(isempty(issues), '╶', '╭'), "─● ")
-        printstyled(io, String(label), bold=true, color=:light_cyan)
-        print(io, '\n')
+        print(io, '\n', ifelse(isempty(issues), '╶', '╭'), "─● ", styled"{bold,bright_cyan:$label}")
         for (i, issue) in enumerate(sort(issues, by=i->i.severity))
-            color = get(SEVERITY_COLORS, issue.severity, :light_black)
+            face = get(SEVERITY_FACES, issue.severity, :light_black)
             label = get(SEVERITY_LABELS, issue.severity, "Unknown")
-            print(io, "│ ")
-            printstyled(io, "┌ "; color)
-            printstyled(io, "$i $label: "; color, bold=true)
-            printstyled(io, String(issue.label); color)
-            print(io, "\n│ ")
-            printstyled(io, "│ "; color)
-            print(io, issue.issue)
-            print(io, ifelse(i == length(issues), "\n╰ ", "\n│ "))
-            printstyled(io, "└"; color)
-            if !isnothing(issue.source)
-                printstyled(" @ $(issue.source.file):$(issue.source.line)",
-                            color=:light_black)
-            else
-                printstyled(" @ unknown", color=:light_black)
-            end
-            print(io, '\n')
+            srcloc = if isnothing(issue.source) "unknown" else
+                string(issue.source.file, ':', issue.source.line) end
+            print(io, '\n',
+                  styled"│ {$face:┌ {bold:$i $label:} $(issue.label)}\n\
+                         │ {$face:│} $(issue.issue)\n$(ifelse(i == length(issues), '╰', '│')) \
+                           {$face:└} {grey:@ $srcloc}")
         end
     end
 end
